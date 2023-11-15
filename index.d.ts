6ca14c73879b18f3db0884387d3fdcad1285ad20
강슬기 <mydog21@gmail.com>
2023-11-15 15:25:46 +0900

    1.2.6 typescript support

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,12 @@
-export = KoreanIme;
-
-declare function KoreanIme(prev: string, input: string ): string;
-declare function engToKor(value : string): string;
-declare function korToEng(value : string): string;
-declare function destructiveKorean(value : string): string[];
-
-export = engToKor;
-export = korToEng;
-export = destructiveKorean;
+declare module 'korean-ime' {
+  export = KoreanIme;
+  
+  declare function KoreanIme(prev: string, input: string ): string;
+  declare function engToKor(value : string): string;
+  declare function korToEng(value : string): string;
+  declare function destructiveKorean(value : string): string[];
+  
+  export = engToKor;
+  export = korToEng;
+  export = destructiveKorean;
+}
